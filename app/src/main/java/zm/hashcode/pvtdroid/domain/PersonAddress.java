7e9a0dacd49718f71b464183d8f4949ead02948a
hashcode <boniface@hashcode.zm>
2016-04-06 20:02:08 +0200

    Initial Committ

---
+++ app/src/main/java/zm/hashcode/pvtdroid/domain/PersonAddress.java
@@ -0,0 +1,14 @@
+package zm.hashcode.pvtdroid.domain;
+
+import java.util.Date;
+
+/**
+ * Created by hashcode on 2016/04/06.
+ */
+public class PersonAddress {
+    private Long id;
+    private String description;
+    private String postalCode;
+    private String addressTypeId;
+    private Date date;
+}
---
+++ app/src/main/java/zm/hashcode/pvtdroid/domain/PersonContact.java
@@ -0,0 +1,15 @@
+package zm.hashcode.pvtdroid.domain;
+
+import java.util.Date;
+
+/**
+ * Created by hashcode on 2016/04/06.
+ */
+public class PersonContact {
+    private Long id;
+    private String contactTypeId;
+    private String contactValue;
+    private String status;
+    private Date date;
+    private String state;
+}
